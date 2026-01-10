22eb1960d3e30fd46e56923685b2565cbac72560
ram-globalmagicko <ram@globalmagicko.com>
2026-01-10 13:22:50 +0700

    Added the font configuration

--- src/components/hero/navigation.tsx
+++ src/components/hero/navigation.tsx
@@ -1,7 +1,7 @@
 export const Navigation = () => {
   return (
     <div className="py-6 flex justify-between">
-      <h2 className="text-white">Albert</h2>
+      <h2 className="text-white text-5xl font-extrabold font-sans">Albert</h2>
       <ul className="flex gap-2">
         <li>Resume</li>
         <li>Projects</li>
---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          "Bricolage Grotesque",
+          "ui-sans-serif",
+          "system-ui",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
