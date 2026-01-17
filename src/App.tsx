7f69be6dd4b31f3ed5d104b6a546bf812f3a38c9
ram-globalmagicko <ram@globalmagicko.com>
2026-01-17 10:03:10 +0700

    Added the motion integration with the lenis

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import { ReactLenis } from "lenis/react";
+import { useEffect, useRef } from "react";
+import { ReactLenis, type LenisRef } from "lenis/react";
+import { frame, cancelFrame } from "motion";
 import { Hero } from "@/components/hero";
 import { Skill } from "@/components/skill";
 import { About1 } from "./components/about-1";
 
 export default function App() {
+  const lenisRef = useRef<LenisRef>(null)
+
+  useEffect(() => {
+    function update(data: { timestamp: number }) {
+      const time = data.timestamp
+      lenisRef.current?.lenis?.raf(time)
+    }
+
+    frame.update(update, true)
+
+    return () => cancelFrame(update)
+  }, [])
+
   return (
     <ReactLenis
       root
@@ -11,6 +26,7 @@ export default function App() {
         duration: 1.2,
         easing: (t) => Math.min(1, 1 - Math.pow(1 - t, 4)),
       }}
+      ref={lenisRef}
     >
       <Hero />
       <Skill />
