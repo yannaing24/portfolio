0e091a8c2d5e1d15cc1add31e495fcd89fac95c5
ram-globalmagicko <ram@globalmagicko.com>
2026-02-14 14:12:13 +0700

    Updated the carousel changes

--- src/components/review/carousel.tsx
+++ src/components/review/carousel.tsx
@@ -1,86 +1,84 @@
-import { useState, useEffect, useEffectEvent } from "react"
-import { Card, CardContent } from "@/components/ui/card"
+import { useState, useEffect, useEffectEvent } from "react";
+import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel as SCarousel,
   CarouselContent,
   CarouselItem,
-  type CarouselApi
-} from "@/components/ui/carousel"
-import { data } from './data'
-import { Button } from "../ui/button"
-import { ArrowLeft, ArrowRight } from "lucide-react"
-
-
+  type CarouselApi,
+} from "@/components/ui/carousel";
+import { data } from "./data";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 
 export const Carousel = () => {
-    const [api, setApi] = useState<CarouselApi>()
-  const [current, setCurrent] = useState(0)
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
 
- const update = useEffectEvent((api: CarouselApi) => {
-    if(api === undefined) return;
-    setCurrent(api.selectedScrollSnap() + 1)
+  const update = useEffectEvent((api: CarouselApi) => {
+    if (api === undefined) return;
+    setCurrent(api.selectedScrollSnap() + 1);
     api.on("select", () => {
-      setCurrent(api.selectedScrollSnap() + 1)
-    })
-})
-   
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  });
+
   useEffect(() => {
     if (!api) {
-      return
+      return;
     }
-    update(api)
-    
-  }, [api])
-  
+    update(api);
+  }, [api]);
 
   const handlePrevious = () => {
-    if(api === undefined) return
-    api.scrollPrev()
-}
+    if (api === undefined) return;
+    api.scrollPrev();
+  };
 
   const handleNext = () => {
-    if(api === undefined) return
-    api.scrollNext()
-}
+    if (api === undefined) return;
+    api.scrollNext();
+  };
 
-    return <div className="py-16">
-        <SCarousel
-            opts={{
-                align: "start",
-                loop: true,
-            }}
-            className="w-full"
-            setApi={setApi}
-        >
+  return (
+    <div className="py-16">
+      <SCarousel
+        opts={{
+          align: "start",
+          loop: true,
+        }}
+        className="w-full"
+        setApi={setApi}
+      >
         <CarouselContent>
-            {data.map((_, index) => (
+          {data.map((_, index) => (
             <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/4">
-                <div className="p-1">
+              <div className="p-1">
                 <Card>
-                    <CardContent className="flex aspect-square items-center justify-center p-2">
-                    <img
-                     src={data[index].img}
-                     className="object-cover"
-                      />
-                    </CardContent>
+                  <CardContent className="flex aspect-square items-center justify-center p-2">
+                    <img src={data[index].img} className="object-cover" />
+                  </CardContent>
                 </Card>
-                </div>
+              </div>
             </CarouselItem>
-            ))}
+          ))}
         </CarouselContent>
-        </SCarousel>
-       <div className="py-8 flex justify-between">
-            <div>
-                <p>{data[current-1]?.content}</p>
-                <p>{data[current-1]?.name}</p>
-            </div>
-            <div className="flex gap-2">
-                <Button onClick={handlePrevious}>
-                <ArrowLeft className="h-4 w-4" />
-                </Button>
-                <Button onClick={handleNext}><ArrowRight className="h-4 w-4"/></Button>
-            </div>
+      </SCarousel>
+      <div className="py-8 flex justify-between items-start">
+        <div>
+          <h3 className="text-[28px] mb-2">{data[current - 1]?.content}</h3>
+          <p className="text-[20px]">{data[current - 1]?.name}</p>
         </div>
+        <div className="flex gap-2">
+          <div
+            className="p-4 bg-yellow-300 rounded-xl"
+            onClick={handlePrevious}
+          >
+            <ArrowLeft size={24} />
+          </div>
+          <div className="p-4 bg-yellow-300 rounded-xl" onClick={handleNext}>
+            <ArrowRight size={24} />
+          </div>
+        </div>
+      </div>
     </div>
-
-}
+  );
+};
